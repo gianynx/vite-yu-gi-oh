f467249b539044b02cad9a8bc838f1779f4c96a2
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-17 16:58:33 +0200

    create project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>CIAO</h1>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss" scoped>
+* {
+  font-family: 'Kanit', sans-serif;
+}
+</style>
