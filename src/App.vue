11b4ec5bb98ddb6a6333f093143b0e96ff74372c
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-17 17:33:19 +0200

    add getCard() inside App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,24 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getCard() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0').then((res) => {
+        console.log(res.data.results);
+      });
+    }
+  },
+  mounted() {
+    this.getCard();
+  }
 }
 </script>
 
